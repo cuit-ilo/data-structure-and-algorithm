a0e7784aedf1db79805629936695bb8366cab9df
cuit-ilo <ilocuit39@gmail.com>
2024-11-14 00:32:16 +0100

    Add files via upload

---
+++ Binarysearchtreeinsert.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.binarysearchtreeinsert;
+
+/**
+ *
+ * @author HP
+ */
+public class Binarysearchtreeinsert {
+
+    public static void main(String[] args) {
+       
+    }
+}
